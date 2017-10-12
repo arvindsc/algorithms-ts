179ef1aa9801f6ab706577d1c788589c870367e7
attachesoftware\ArvindC <arvindc@attachesoftware.com>
2017-10-12 17:45:38 +1100

    recursive implementation of factorial function

--- src/mathworld.ts
+++ src/mathworld.ts
@@ -1,15 +1,5 @@
 export class MathWorld {
     static factorial(num: number) {
-        let temp = 1;
-      
-        if (num == 0 ) {
-            return 1
-        }
-        while (num > 1) {
-            console.log(num);
-            temp *=num--; 
-        }
-        return temp;
-      
+        return num > 0 ? num * this.factorial(num-1): 1;
     }
 }
